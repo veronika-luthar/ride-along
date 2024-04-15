dfde08f7c9045849865260a8f3ef06282a469b7e
Aidan Cushman <cusai543@student.otago.ac.nz>
2024-04-15 12:56:44 +1200

    fixed controller

--- backend/src/controllers/ride_controller.js
+++ backend/src/controllers/ride_controller.js
@@ -2,17 +2,21 @@ const {User, sequelize} = require('../models');
 
 
 module.exports = {
-    async getRidesByCity(city) {
+    async getRidesByCity(req, res) {
         try {
-          const rides = await Ride.findAll({
-            where: {
-              city: city
-            }
-          });
-          return rides;
+          const city = req.params.city;
+          const rides = await getRidesByCityFromDatabase(city);
+          res.json(rides);
         } catch (error) {
           console.error('Error retrieving rides by city:', error);
-          throw error;
+          res.status(500).json({ error: 'Internal server error' });
         }
-      }
+      },
+
+    
+    async joinRide(req, res) {
+        
+    }
+    // Implement other controller methods for CRUD operations
+
 };
