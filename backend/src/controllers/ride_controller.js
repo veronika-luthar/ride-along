7b532ee1c5479d0424e3fc34e2c17281a2210a3b
Aidan Cushman <cusai543@student.otago.ac.nz>
2024-04-15 13:21:57 +1200

    Added more to the ride controller

--- backend/src/controllers/ride_controller.js
+++ backend/src/controllers/ride_controller.js
@@ -19,6 +19,40 @@ module.exports = {
 
     
     async joinRide(req, res) {
+        try {
+          const rideID = req.params.rideID;
+          const ride = await Ride.findAll({
+            where: {
+                ride_id: rideID
+            }
+          });
+          if (!ride) {
+            return res.status(404).json({ error: 'Ride not found' });
+          }
+          if (ride.attendance == ride.max_attendance) {
+            return res.status(400).json({ error: 'No seats available' });
+          }
+          ride.attendance += 1;
+          sequelize.query(
+            `UPDATE rides SET attendance = ${ride.attendance} WHERE ride_id = ${rideID}`
+          );
+          //Add ride user to ride attendance table
+          
+          res.status(200);
+        } catch (error) {
+          console.error('Error joining ride:', error);
+          res.status(500).json({ error: 'Internal server error' });
+        }
+      },
+
+    async getRides(req, res) {
+        try {
+          const rides = await Ride.findAll();
+          res.status(200).json(rides);
+        } catch (error) {
+          console.error('Error retrieving rides:', error);
+          res.status(500).json({ error: 'Internal server error' });
+        }
         
     }
     // Implement other controller methods for CRUD operations
