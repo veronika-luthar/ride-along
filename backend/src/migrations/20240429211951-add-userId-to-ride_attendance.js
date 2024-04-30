56e39a3902d63153c45e9ecb190faaa42add86c6
Aidan Cushman <cusai543@student.otago.ac.nz>
2024-04-30 12:41:14 +1200

    fixed name error in migratioj

--- backend/src/migrations/20240429211951-add-userId-to-ride_attendance.js
+++ backend/src/migrations/20240429211951-add-userId-to-ride_attendance.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('RideAttendances', 'isOwnder', {
+    await queryInterface.addColumn('RideAttendances', 'isOwner', {
       type: Sequelize.BOOLEAN,
       allowNull: true,
       onUpdate: 'CASCADE',
@@ -12,6 +12,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('RideAttendances', 'isOwnder');
+    await queryInterface.removeColumn('RideAttendances', 'isOwner');
   }
 };
