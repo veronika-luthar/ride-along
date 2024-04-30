219a530b5e299f894e66ba4b5bbb3ce952c3c0c6
veronika-luthar <justapinglet@gmail.com>
2024-05-01 11:03:21 +1200

    Hoist create ride state

--- frontend/src/components/CreateRide.js
+++ frontend/src/components/CreateRide.js
@@ -3,9 +3,10 @@ import { useState } from "react";
 import '../styles/FormStyles.css';
 import env from "react-dotenv";
 import { useNavigate } from 'react-router-dom'; // Import useHistory
+import RideForm from './RideForm';
 
 
-export default function Form() {
+export default function CreateRide() {
   const navigate = useNavigate();
     const [input, setInput] = useState({
         title: "",
@@ -43,94 +44,11 @@ export default function Form() {
 
     return (
         <>
-            <form onSubmit={handleSubmit} className="form-container">
-                <label>
-                    Title
-                    <input
-                        value={input.title}
-                        onChange={handleChange}
-                        type="text"
-                        id="title"
-                        className="form-input"
-                        required
-                    />
-                </label>
-                <label>
-                    Date
-                    <input
-                        value={input.date}
-                        onChange={handleChange}
-                        type="date"
-                        id="date"
-                        className="form-input"
-                        required
-                    />
-                </label>
-                <label>
-                    Time
-                    <input
-                        value={input.time}
-                        onChange={handleChange}
-                        type="time"
-                        id="time"
-                        className="form-input"
-                        required
-                    />
-                </label>
-                <label>
-                    Estimated duration
-                    <input
-                        value={input.estimated_duration}
-                        onChange={handleChange}
-                        type="time"
-                        id="estimated_duration"
-                        className="form-input"
-                        required
-                    />
-                </label>
-                <label>
-                    City
-                    <input 
-                        value={input.city}
-                        onChange={handleChange}
-                        type="text"
-                        id="city"
-                        className="form-input"
-                        required
-                    />
-                </label>
-                <label>
-                    Start location
-                    <input 
-                        value={input.start_location}
-                        onChange={handleChange}
-                        type="text"
-                        id="start_location"
-                        className="form-input"
-                        required
-                    />
-                </label>
-                <label>
-                    Description
-                    <textarea
-                        value={input.description}
-                        onChange={handleChange}
-                        id="description"
-                        className="form-input"
-                    />
-                </label>
-                <label>
-                    Max attendance
-                    <input
-                        value={input.max_attendance}
-                        onChange={handleChange} 
-                        type="number" 
-                        id="max_attendance"
-                        className="form-input"
-                    />
-                </label>
-                <input type="submit" value="Create ride" className="form-button"></input>
-            </form>
+            <RideForm 
+                input={input}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
         </>
     ); 
 }
