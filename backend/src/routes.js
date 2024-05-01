b13b50994252e1c164fe71bdeb5045e03116dabc
veronika-luthar <justapinglet@gmail.com>
2024-05-02 09:34:51 +1200

    Add backend functionality for editing ride

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -27,6 +27,8 @@ router.get('/cities', ride_controller.getCities);
 router.post('/rides/:rideID/join',authenticateToken, ride_controller.joinRide);
 
 router.post('/create-ride', authenticateToken, ride_controller.createRide);
+router.post('/edit-ride', ride_controller.editRide);
+router.get('is-owner', authenticateToken, ride_controller.isOwner);
 
 router.post('/rides/:rideID/leave',authenticateToken,ride_controller.leaveRide); 
 
