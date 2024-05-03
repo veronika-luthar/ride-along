a531e71d6177cccf7d0866f5e99206101837f8e9
veronika-luthar <justapinglet@gmail.com>
2024-05-04 11:44:21 +1200

    Remove ride form and make create and edit ride forms separate

--- frontend/src/components/EditRide.js
+++ frontend/src/components/EditRide.js
@@ -2,21 +2,15 @@ import axios from "axios";
 import { useState } from "react";
 import '../styles/FormStyles.css';
 import env from "react-dotenv";
-import { useNavigate } from 'react-router-dom'; // Import useHistory
+import { useNavigate, useParams } from 'react-router-dom'; // Import useHistory
 import RideForm from './RideForm';
 
 
 export default function EditRide() {
-  const navigate = useNavigate();
-    const [input, setInput] = useState({
-        date: "",
-        time: "",
-        estimated_duration: "",
-        start_location: "",
-        description: "",
-        max_attendance: "",
-
-    });
+    const navigate = useNavigate();
+    const ride = JSON.parse(localStorage.getItem('ride-edit'));
+    console.log(ride);
+    const [input, setInput] = useState({ ride });
 
     function handleChange(e) {
         setInput({
@@ -27,10 +21,11 @@ export default function EditRide() {
 
     function handleSubmit(e){
         e.preventDefault();
-        axios.post(`${env.BASE_URL}/edit-ride`, input)
+        axios.post(`${env.BASE_URL}/edit-ride`, ride)
             .then(function (response) {
                 if(response.status === 200){
                     alert("Ride Edited!");
+                    localStorage.removeItem('ride-edit');
                     navigate('/');
                 }
             })
@@ -41,11 +36,74 @@ export default function EditRide() {
 
     return (
         <>
-            <RideForm 
-                input={input}
-                onChange={handleChange}
-                onSubmit={handleSubmit}
-            />
+            <h1>{ride.title}</h1>
+            <h2>{ride.city}</h2>
+            <form onSubmit={handleSubmit} className="form-container">
+                <label>
+                    Date
+                    <input
+                        value={ride.date}
+                        onChange={handleChange}
+                        type="date"
+                        id="date"
+                        className="form-input"
+                        required
+                    />
+                </label>
+                <label>
+                    Time
+                    <input
+                        value={ride.time}
+                        onChange={handleChange}
+                        type="time"
+                        id="time"
+                        className="form-input"
+                        required
+                    />
+                </label>
+                <label>
+                    Estimated duration
+                    <input
+                        value={ride.estimatedDuration}
+                        onChange={handleChange}
+                        type="time"
+                        id="estimated_duration"
+                        className="form-input"
+                        required
+                    />
+                </label>
+                <label>
+                    Start location
+                    <input 
+                        value={ride.startLocation}
+                        onChange={handleChange}
+                        type="text"
+                        id="start_location"
+                        className="form-input"
+                        required
+                    />
+                </label>
+                <label>
+                    Description
+                    <textarea
+                        value={ride.description}
+                        onChange={handleChange}
+                        id="description"
+                        className="form-input"
+                    />
+                </label>
+                <label>
+                    Max attendance
+                    <input
+                        value={ride.maxAttendance}
+                        onChange={handleChange} 
+                        type="number" 
+                        id="max_attendance"
+                        className="form-input"
+                    />
+                </label>
+                <input type="submit" value="Create ride" className="form-button"></input>
+            </form>
         </>
     ); 
 }
