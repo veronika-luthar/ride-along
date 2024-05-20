251178c96707afa54b9b0b2c404297c397e80b79
Aidan Cushman <cusai543@student.otago.ac.nz>
2024-05-20 12:18:58 +1200

    changed rating

--- frontend/src/components/Stars.js
+++ frontend/src/components/Stars.js
@@ -1,20 +1,16 @@
 import React from 'react';
 
 const Stars = ({ rating }) => {
-  const maxRating = 5; // Adjust the maximum rating as needed
+  const maxRating = 1; // Adjust the maximum rating as needed
 
   const getStarIcon = (index) => {
-    if (index < rating) {
-      return '★'; // Filled star
-    } else {
       return '☆'; // Empty star
-    }
   };
 
   return (
     <div style={{ display: 'inline' }}>
       {[...Array(maxRating)].map((_, index) => (
-        <span key={index}>{getStarIcon(index)}</span>
+        <span key={index}>{getStarIcon(index)}{rating}</span>
       ))}
     </div>
   );
