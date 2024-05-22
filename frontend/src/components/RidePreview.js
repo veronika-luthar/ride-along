d6aec851f0309ba7f7bc5b231bdcbfc30eefb5ac
Aidan Cushman <cusai543@student.otago.ac.nz>
2024-05-22 14:57:19 +1200

    Added rating logic

--- frontend/src/components/RidePreview.js
+++ frontend/src/components/RidePreview.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import '../styles/RidePreview.css'; // Import the CSS file
-
+import { useEffect,useState } from 'react';
 const RidePreview = ({ ride, onSelectRide }) => {
   console.log(ride);
   const { city, createdAt, description, id, maxAttendance, date, time, startLocation, title, updatedAt } = ride;
   const formattedScheduledTime = `${time}`;
+  const [showRateRideButton,setShowRateRide] = useState(false);
+
+
+  useEffect(() => {
+
+    const shouldShowRateRide =  async () => { 
+        const currentTime = new Date();
+        const date1 = new Date(date)
+        const [hours, minutes] = time.split(':');
+        const rideTime = new Date(date1.getFullYear(), date1.getMonth(), date1.getDate(), hours, minutes);
+        console.log(time + ' current:' + rideTime);
+        if (rideTime < currentTime) {
+          setShowRateRide(true);
+        }
+    }
+    shouldShowRateRide()
+    })
+
+
+    const rateRideClicked = () => {
+        console.log('Rate ride clicked');
+        onSelectRide(ride);
+    }
 
   return (
     <div className="ride-preview">
@@ -13,10 +36,19 @@ const RidePreview = ({ ride, onSelectRide }) => {
           <p className="ride-preview-title">{ride.title}</p>
           <p className="ride-preview-city">{ride.city}</p>
         </div>
+        {showRateRideButton && (
+        <div>
+        <button className="form-button" onClick={rateRideClicked}>
+        Rate Ride!!
+        </button>
+        </div>
+        )}
+        {!showRateRideButton && (
         <div className="ride-preview-date-time">
           <p>{ride.date}</p>
           <p>{formattedScheduledTime}</p>
         </div>
+        )}
       </div>
       <p className="ride-preview-description">{ride.description}</p>
     </div>
@@ -24,13 +56,9 @@ const RidePreview = ({ ride, onSelectRide }) => {
 };
 /*
       {showRateRideButton && (
-        <div className="attendance-popup">
-          <div className="attendance-popup-content">
-            <RateRider rideID={id}/>
-            <button className="form-button" onClick={toggleRateRidePopup}>
-              Close
-            </button>
-          </div>
+        <div>
+        <button className="form-button" onClick={rateRideClicked}>
+        </button>
         </div>
 */
 
